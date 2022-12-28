003fae3c1eb7befddced6c835e13e47ae1cbc5ff
Robb Lovell <robbl@vidigami.com>
2022-12-28 14:42:33 -0800

    fix: remove quotes from quoted path ids

--- src/get-service.js
+++ src/get-service.js
@@ -9,7 +9,11 @@ module.exports = function getService (feathersApp, path) {
   if (!feathersApp.service(service)) {
     feathersId = pathArray.pop()
     if (!isNaN(feathersId)) {
+      // Test for a number for ids that are numbers.
       feathersId = parseInt(feathersId)
+    } else if ((feathersId[0] === '"' && feathersId[feathersId.length - 1] === '"') || (feathersId[0] === "'" && feathersId[feathersId.length - 1] === "'")) {
+      // For ids that are type string but look like numbers, a workaround is to quote them in the path.
+      feathersId = feathersId.slice(1, -1) // Remove quotes from quoted path ids.
     }
     service = pathArray.join('/')
   }
